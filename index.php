74cec01da71328a98c21ce0867e2f7d61142a0f9
Nzingani <isabelnzingani@outlook.com>
2018-07-26 15:22:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exercice3</title>
+  </head>
+  <body>
+    <p>
+<?php
+$gender = 'women';
+
+if($gender == 'women')
+{
+echo 'C\'est une développeuse !!!';
+}
+elseif($ $gender == 'men')
+{
+echo 'C\'est un développeur !!!';
+}
+?>
+</p>
+</body>
